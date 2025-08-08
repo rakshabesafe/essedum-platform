b5656e01623a6f7da4267844eb38b0a922fe358c
Dharmasen Yadav <84275605+Dharmasen@users.noreply.github.com>
2025-08-08 13:15:32 +0530

    added backend file

--- sv/icip-lib-iai/src/main/java/com/infosys/icets/icip/icipwebeditor/repository/ICPMLFederatedModelsDSRepository.java
+++ sv/icip-lib-iai/src/main/java/com/infosys/icets/icip/icipwebeditor/repository/ICPMLFederatedModelsDSRepository.java
@@ -12,6 +12,7 @@ import org.springframework.data.repository.query.Param;
 
 import com.infosys.icets.icip.icipwebeditor.model.ICIPMLFederatedModel;
 import com.infosys.icets.icip.icipwebeditor.model.ICIPMLFederatedModelDS;
+import com.infosys.icets.icip.icipwebeditor.model.dto.ICIPDatasourceFilterDTO;
 
 @NoRepositoryBean
 public interface ICPMLFederatedModelsDSRepository extends JpaRepository<ICIPMLFederatedModelDS, Integer> {
@@ -41,30 +42,53 @@ public interface ICPMLFederatedModelsDSRepository extends JpaRepository<ICIPMLFe
 	List<ICIPMLFederatedModelDS> getModelByModelNameAndOrganisation(@Param("modelName") String fedName,
 			@Param("org") String org);	
 	
-	
-	@Query("SELECT model FROM ICIPMLFederatedModel model " +
-		       "WHERE model.organisation = :organisation AND " +
-		       "(:modelNames IS NULL OR model.modelName IN :modelNames) AND " +
-		       "(:versions IS NULL OR model.version IN :versions) AND " +
-		       "(:modelTypes IS NULL OR model.modelType IN :modelTypes) " +
+	@Query("SELECT model FROM ICIPMLFederatedModelDS model " +
+		       "WHERE model.datasource.organization = :organisation AND " +
+		       "(:datasourceNames IS NULL OR model.datasource.name IN :datasourceNames) AND " +
+		       "(" +
+		       "  :searchValue IS NULL OR " +
+		       "  LOWER(model.modelName) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.description) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.version) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.modelType) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.createdBy) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.modifiedBy) LIKE LOWER(CONCAT('%', :searchValue, '%'))" +
+		       ")" +
 		       "ORDER BY model.modifiedDate DESC")
-		Page<ICIPMLFederatedModelDS> findByOrganisationAndOptionalParams(
+		Page<ICIPMLFederatedModelDS> findByOrganisationAndOptionalDatasourceNamesAndSearch(
 		    @Param("organisation") String organisation,
-		    @Param("modelNames") List<String> modelNames,
-		    @Param("versions") List<String> versions,
-		    @Param("modelTypes") List<String> modelTypes,
+		    @Param("datasourceNames") List<String> datasourceNames,
+		    @Param("searchValue") String searchValue,
 		    Pageable pageable
 		);
 
-	@Query("SELECT COUNT(model) FROM ICIPMLFederatedModel model " +
-		       "WHERE model.organisation = :organisation AND " +
-		       "(:modelNames IS NULL OR model.modelName IN :modelNames) AND " +
-		       "(:versions IS NULL OR model.version IN :versions) AND " +
-		       "(:modelTypes IS NULL OR model.modelType IN :modelTypes)")
-		Long countByOrganisationAndOptionalParams(
+
+	@Query("SELECT COUNT(model) FROM ICIPMLFederatedModelDS model " +
+		       "WHERE model.datasource.organization = :organisation AND " +
+		       "(:datasourceNames IS NULL OR model.datasource.name IN :datasourceNames) AND " +
+		       "(" +
+		       "  :searchValue IS NULL OR " +
+		       "  LOWER(model.modelName) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.description) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.version) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.modelType) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.createdBy) LIKE LOWER(CONCAT('%', :searchValue, '%')) OR " +
+		       "  LOWER(model.modifiedBy) LIKE LOWER(CONCAT('%', :searchValue, '%'))" +
+		       ")")
+		Long countByOrganisationAndOptionalDatasourceNamesAndSearch(
 		    @Param("organisation") String organisation,
-		    @Param("modelNames") List<String> modelNames,
-		    @Param("versions") List<String> versions,
-		    @Param("modelTypes") List<String> modelTypes
+		    @Param("datasourceNames") List<String> datasourceNames,
+		    @Param("searchValue") String searchValue
 		);
+
+	@Query("SELECT new com.infosys.icets.icip.icipwebeditor.model.dto.ICIPDatasourceFilterDTO(model.datasource.name, model.datasource.alias) " +
+		       "FROM ICIPMLFederatedModelDS model " +
+		       "WHERE model.datasource.organization = :organisation " +
+		       "GROUP BY model.datasource.name, model.datasource.alias")
+		List<ICIPDatasourceFilterDTO> findDistinctDatasourceNameAndAliasByOrganisation(
+		    @Param("organisation") String organisation
+		);
+
+
+
 }
